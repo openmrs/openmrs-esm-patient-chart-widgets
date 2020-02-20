2d6a4de82cbc40dd941dbb3a7d51e8b65cb78c99
Jonathan Dick <jdick@ampath.or.ke>
2020-02-20 21:39:39 +0300

    self-review

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = env => ({
             options: {
               modules: {
                 localIdentName:
-                  "esm-patient-chart__[name]__[local]___[hash:base64:5]"
+                  "esm-patient-chart-widgets__[name]__[local]___[hash:base64:5]"
               }
             }
           }
