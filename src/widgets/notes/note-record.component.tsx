b975cb960784d015fa1418f675ef9e5e331cfc77
Dennis Kigen <kigen.work@gmail.com>
2020-03-20 22:14:24 +0300

    Improvements to NoteRecord component

--- src/widgets/notes/note-record.component.tsx
+++ src/widgets/notes/note-record.component.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import { useRouteMatch } from "react-router-dom";
 import { useCurrentPatient } from "@openmrs/esm-api";
 import { fetchEncounterByUuid } from "./encounter.resource";
 import { createErrorHandler } from "@openmrs/esm-error-handling";
+import styles from "./note-record.css";
+import SummaryCard from "../../ui-components/cards/summary-card.component";
+import dayjs from "dayjs";
 
 export default function NoteRecord(props: NoteRecordProps) {
   const [note, setNote] = useState(null);
@@ -19,7 +22,76 @@ export default function NoteRecord(props: NoteRecordProps) {
     }
   }, [isLoadingPatient, patient, match.params]);
 
-  return <>NoteRecord works!</>;
+  function displayNote() {
+    return (
+      <>
+        {note && (
+          <SummaryCard name="Note" styles={{ width: "100%" }}>
+            <div className={`omrs-type-body-regular ${styles.noteCard}`}>
+              <div>
+                <p className="omrs-type-title-3">{note?.display}</p>
+              </div>
+              <table className={styles.noteTable}>
+                <thead>
+                  <tr>
+                    <td>Encounter type</td>
+                    <td>Location</td>
+                    <td>Encounter datetime</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{note?.encounterType?.display}</td>
+                    <td>{note?.location?.display}</td>
+                    <td>
+                      {dayjs(note?.encounterDatetime).format("DD-MM-YYYY")}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </SummaryCard>
+        )}
+      </>
+    );
+  }
+
+  function displayNoteDetails() {
+    return (
+      <>
+        {note.obs && note.obs.length && (
+          <SummaryCard
+            name="Details"
+            styles={{
+              width: "100%",
+              backgroundColor: "var(--omrs-color-bg-medium-contrast)"
+            }}
+          >
+            <div className={`omrs-type-body-regular ${styles.noteCard}`}>
+              {note.obs.map(ob => {
+                return (
+                  <Fragment key={ob.uuid}>
+                    <p>{ob.display}</p>
+                  </Fragment>
+                );
+              })}
+            </div>
+          </SummaryCard>
+        )}
+      </>
+    );
+  }
+
+  return (
+    <div className={styles.noteContainer}>
+      {note && <div className={styles.noteSummary}>{displayNote()}</div>}
+      {note && note.obs.length ? (
+        <div className={styles.noteSummary}>{displayNoteDetails()}</div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 type NoteRecordProps = {};
--- src/widgets/notes/note-record.test.tsx
+++ src/widgets/notes/note-record.test.tsx
@@ -5,6 +5,10 @@ import NoteRecord from "./note-record.component";
 import { mockPatient } from "../../../__mocks__/patient.mock";
 import { useCurrentPatient } from "../../../__mocks__/openmrs-esm-api.mock";
 import { fetchEncounterByUuid } from "./encounter.resource";
+import {
+  mockEncounterResponse,
+  mockAlternativeEncounterResponse
+} from "../../../__mocks__/encounters.mock";
 import { of } from "rxjs/internal/observable/of";
 
 const mockUseCurrentPatient = useCurrentPatient as jest.Mock;
@@ -30,10 +34,10 @@ describe("<NoteRecord />", () => {
       mockPatient.id,
       null
     ]);
-    mockFetchPatientEncounter.mockReturnValue(of([]));
   });
 
   it("renders without dying", async () => {
+    mockFetchPatientEncounter.mockReturnValue(of(mockEncounterResponse));
     wrapper = render(
       <BrowserRouter>
         <NoteRecord />
@@ -42,6 +46,115 @@ describe("<NoteRecord />", () => {
 
     await wait(() => {
       expect(wrapper).toBeDefined();
+    });
+  });
+
+  it("displays a detailed summary of the selected note", async () => {
+    mockFetchPatientEncounter.mockReturnValue(of(mockEncounterResponse));
+    wrapper = render(
+      <BrowserRouter>
+        <NoteRecord />
+      </BrowserRouter>
+    );
+
+    await wait(() => {
+      expect(wrapper).toBeDefined();
+      expect(wrapper.getByText("Note").textContent).toBeTruthy();
+      expect(
+        wrapper.getByText("Visit Note 28/01/2015").textContent
+      ).toBeTruthy();
+      expect(wrapper.getByText("Encounter type").textContent).toBeTruthy();
+      expect(wrapper.getByText("Encounter datetime").textContent).toBeTruthy();
+      expect(wrapper.getByText("Location").textContent).toBeTruthy();
+      expect(wrapper.getByText("Visit Note").textContent).toBeTruthy();
+      expect(wrapper.getByText("Unknown Location").textContent).toBeTruthy();
+      expect(wrapper.getByText("28-01-2015").textContent).toBeTruthy();
+      expect(wrapper.getByText("Details").textContent).toBeTruthy();
+      expect(
+        wrapper.getByText(
+          "Visit Diagnoses: Presumed diagnosis, Primary, Vitamin A Deficiency with Keratomalacia"
+        ).textContent
+      ).toBeTruthy();
+      expect(
+        wrapper.getByText(
+          "Visit Diagnoses: Other disease of hard tissue of teeth, Secondary, Confirmed diagnosis"
+        ).textContent
+      ).toBeTruthy();
+      expect(
+        wrapper.getByText(
+          "Text of encounter note: Duis aute irure dolor in reprehenderit in voluptat"
+        ).textContent
+      ).toBeTruthy();
+    });
+  });
+
+  it("displays a detailed summary of the selected note", async () => {
+    mockFetchPatientEncounter.mockReturnValue(of(mockEncounterResponse));
+    wrapper = render(
+      <BrowserRouter>
+        <NoteRecord />
+      </BrowserRouter>
+    );
+
+    await wait(() => {
+      expect(wrapper).toBeDefined();
+      expect(wrapper.getByText("Note").textContent).toBeTruthy();
+      expect(
+        wrapper.getByText("Visit Note 28/01/2015").textContent
+      ).toBeTruthy();
+      expect(wrapper.getByText("Encounter type").textContent).toBeTruthy();
+      expect(wrapper.getByText("Encounter datetime").textContent).toBeTruthy();
+      expect(wrapper.getByText("Location").textContent).toBeTruthy();
+      expect(wrapper.getByText("Visit Note").textContent).toBeTruthy();
+      expect(wrapper.getByText("Unknown Location").textContent).toBeTruthy();
+      expect(wrapper.getByText("28-01-2015").textContent).toBeTruthy();
+      expect(wrapper.getByText("Details").textContent).toBeTruthy();
+      expect(
+        wrapper.getByText(
+          "Visit Diagnoses: Presumed diagnosis, Primary, Vitamin A Deficiency with Keratomalacia"
+        ).textContent
+      ).toBeTruthy();
+      expect(
+        wrapper.getByText(
+          "Visit Diagnoses: Other disease of hard tissue of teeth, Secondary, Confirmed diagnosis"
+        ).textContent
+      ).toBeTruthy();
+      expect(
+        wrapper.getByText(
+          "Text of encounter note: Duis aute irure dolor in reprehenderit in voluptat"
+        ).textContent
+      ).toBeTruthy();
+    });
+  });
+
+  it("displays the patient note details when present", async () => {
+    mockFetchPatientEncounter.mockReturnValue(
+      of(mockAlternativeEncounterResponse)
+    );
+    wrapper = render(
+      <BrowserRouter>
+        <NoteRecord />
+      </BrowserRouter>
+    );
+
+    await wait(() => {
+      expect(wrapper).toBeDefined();
+      expect(wrapper.getByText("Details").textContent).toBeTruthy();
+      expect(wrapper.getByText("Weight (kg): 65.0").textContent).toBeTruthy();
+      expect(wrapper.getByText("Height (cm): 180.0").textContent).toBeTruthy();
+      expect(
+        wrapper.getByText("Systolic blood pressure: 120.0").textContent
+      ).toBeTruthy();
+      expect(wrapper.getByText("Pulse: 60.0").textContent).toBeTruthy();
+      expect(
+        wrapper.getByText("Blood oxygen saturation: 92.0").textContent
+      ).toBeTruthy();
+      expect(
+        wrapper.getByText("Temperature (C): 37.0").textContent
+      ).toBeTruthy();
+      expect(
+        wrapper.getByText("Diastolic blood pressure: 80.0").textContent
+      ).toBeTruthy();
     });
   });
 });
