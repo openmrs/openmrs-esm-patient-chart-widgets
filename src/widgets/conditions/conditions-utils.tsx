10afe9c66d0d19740b74795e71d75a4118ef3c67
Dennis Kigen <kigen.work@gmail.com>
2020-04-23 18:50:11 +0300

    Review feedback

--- src/widgets/conditions/conditions-utils.tsx
+++ src/widgets/conditions/conditions-utils.tsx
@@ -8,7 +8,34 @@ export const openConditionsWorkspaceTab = (componentToAdd, componentName) => {
       match: { params: {} }
     },
     inProgress: false,
-    validations: (workspaceTabs: any[]) =>
+    validations: (workspaceTabs: Array<{ component: React.FC }>) =>
       workspaceTabs.findIndex(tab => tab.component === componentToAdd)
+  });
+};
+
+export const openEditConditionsWorkspaceTab = (
+  componentName,
+  title,
+  conditionUuid,
+  name,
+  clinicalStatus,
+  onsetDateTime
+) => {
+  newWorkspaceItem({
+    component: componentName,
+    name: title,
+    props: {
+      match: {
+        params: {
+          conditionUuid,
+          name,
+          clinicalStatus,
+          onsetDateTime
+        }
+      }
+    },
+    inProgress: false,
+    validations: (workspaceTabs: Array<{ component: React.FC }>) =>
+      workspaceTabs.findIndex(tab => tab.component === componentName)
   });
 };
