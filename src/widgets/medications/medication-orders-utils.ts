368f1e57c2649162dba1f0232573164c3609e533
Dennis Kigen <kigen.work@gmail.com>
2020-04-24 12:05:02 +0300

    MF-184-medications: Make newWorkspaceItem reusable across the medications widget

--- src/widgets/medications/medication-orders-utils.ts
+++ src/widgets/medications/medication-orders-utils.ts
@@ -68,49 +68,6 @@ export function setDefaultValues(commonDrugOrders) {
   ];
 }
 
-export const openMedicationWorkspaceTab = (componentName, title) => {
-  newWorkspaceItem({
-    component: componentName,
-    name: title,
-    props: {
-      match: {
-        params: {
-          orderUuid: null,
-          drugName: null,
-          action: "NEW"
-        }
-      }
-    },
-    inProgress: false,
-    validations: (workspaceTabs: any[]) =>
-      workspaceTabs.findIndex(tab => tab.component === componentName)
-  });
-};
-
-export const openEditMedicationWorkspaceTab = (
-  componentName,
-  title,
-  orderUuid,
-  drugName
-) => {
-  newWorkspaceItem({
-    component: componentName,
-    name: title,
-    props: {
-      match: {
-        params: {
-          orderUuid: orderUuid,
-          drugName: drugName,
-          action: "REVISE"
-        }
-      }
-    },
-    inProgress: false,
-    validations: (workspaceTabs: any[]) =>
-      workspaceTabs.findIndex(tab => tab.component === componentName)
-  });
-};
-
 export const formatDuration = medication => {
   if (medication) {
     const { duration, durationUnits } = medication;
