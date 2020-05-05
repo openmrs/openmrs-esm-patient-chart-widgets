3a4a8ac700d0542fc67f3e036bc6be04348791b0
Ivange  Larry <ivangelarry@gmail.com>
2020-05-05 01:41:47 +0100

    Add delete button to delete attachments

--- src/widgets/attachments/attachments-overview.component.tsx
+++ src/widgets/attachments/attachments-overview.component.tsx
@@ -10,6 +10,7 @@ import styles from "./attachments-overview.css";
 
 export default function AttachmentsOverview() {
   const [attachments, setAttachments] = useState([]);
+  const [currentImage, setCurrentImage] = useState(0);
 
   const [
     isLoadingPatient,
@@ -24,6 +25,7 @@ export default function AttachmentsOverview() {
       getAttachments(patientUuid, true, abortController).then(
         (response: any) => {
           const listItems = response.data.results.map(attachment => ({
+            id: `${attachment.uuid}`,
             src: `/openmrs/ws/rest/v1/attachment/${attachment.uuid}/bytes`,
             thumbnail: `/openmrs/ws/rest/v1/attachment/${attachment.uuid}/bytes`,
             thumbnailWidth: 320,
@@ -50,6 +52,7 @@ export default function AttachmentsOverview() {
           abortController
         ).then(response => {
           const new_attachment = {
+            id: `${response.data.uuid}`,
             src: `/openmrs/ws/rest/v1/attachment/${response.data.uuid}/bytes`,
             thumbnail: `/openmrs/ws/rest/v1/attachment/${response.data.uuid}/bytes`,
             thumbnailWidth: 320,
@@ -63,8 +66,36 @@ export default function AttachmentsOverview() {
     }
   }
 
+  function handleCurrentImageChange(index) {
+    setCurrentImage(index);
+  }
+
+  function handleDelete() {
+    if (window.confirm("Are you sure you want to delete attachment?")) {
+      const abortController = new AbortController();
+      const id = attachments[currentImage].id;
+      deleteAttachment(
+        id,
+        abortController
+      ).then(response => {
+        const attachments_tmp = attachments.filter(att => att.id != id);
+        setAttachments(attachments_tmp);
+      });
+    }
+  }
+
+  function handleDragOver(e: React.SyntheticEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
   return (
-    <div className={styles.overview}>
+    <div 
+      className={styles.overview}
+      onPaste={e => handleUpload(e, e.clipboardData.files)}
+      onDrop={e => handleUpload(e, e.dataTransfer.files)}
+      onDragOver={handleDragOver}
+    >
       <div className={styles.upload}>
         <form>
           <label htmlFor="fileUpload" className={styles.uploadLabel}>
@@ -78,7 +109,14 @@ export default function AttachmentsOverview() {
           />
         </form>
       </div>
-      <Gallery images={attachments} />
+      <Gallery 
+        images={attachments}
+        enableImageSelection={false}
+        currentImageWillChange={handleCurrentImageChange}
+
+        customControls={[
+          <button key="deleteAttachment" onClick={handleDelete}>Delete</button>
+        ]}/>
     </div>
   );
 }
