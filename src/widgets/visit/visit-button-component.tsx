2bf70e3b7d491a931b701fceb115876d909aaf10
Aashu Rani <aashu.rani@thoughtworks.com>
2020-05-21 14:20:44 +0530

    MF-223 Add Tests for Visit Button component

--- src/widgets/visit/visit-button-component.tsx
+++ src/widgets/visit/visit-button-component.tsx
@@ -63,6 +63,7 @@ export default function VisitButton(props: VisitButtonProps) {
       <div>
         <button
           className={styles.startVisitButton}
+          data-testid="start-visit"
           onClick={() => {
             openVisitDashboard();
             setVisitStarted(true);
@@ -87,6 +88,7 @@ export default function VisitButton(props: VisitButtonProps) {
           {isEmpty(selectedVisit.visitData.stopDatetime) && (
             <button
               className={styles.editVisitButton}
+              data-testid="end-visit"
               onClick={() => {
                 newModalItem({
                   component: endVisit(selectedVisit),
@@ -181,7 +183,7 @@ const cancelActiveVisit = (currentVisit: any): React.ReactNode => {
 
 const endVisit = (currentVisit: any): React.ReactNode => {
   return (
-    <div className={styles.visitPromptContainer} id="end-visit-prompt">
+    <div className={styles.visitPromptContainer} data-testid="end-visit-prompt">
       <h2>Are you sure to end this visit</h2>
       <p>
         Visit Type : {currentVisit.visitData.visitType.display} Location :{" "}
