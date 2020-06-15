4e391c64079eaf3103e0a9db4430edcc1dde000a
Gayatri-Kodag <gayatri.kodag@thoughtworks.com>
2020-06-15 16:16:35 +0530

    Changed endVisit to a React Component

--- __mocks__/patient-visits.mock.ts
+++ __mocks__/patient-visits.mock.ts
@@ -48,19 +48,3 @@ export const mockPatientEndedVisitsResponse = {
     results: patientEndedVisits
   }
 };
-
-export const mockCurrentVisit = {
-  visitData: {
-    visitType: {
-      uuid: "some-uuid1",
-      name: "Outpatient Visit",
-      display: "Outpatient Visit"
-    },
-    startDatetime: new Date(),
-    location: {
-      uuid: "b1a8b05e-3542-4037-bbd3-998ee9c40574",
-      display: "Inpatient Ward",
-      name: "Inpatient Ward"
-    }
-  }
-};
--- src/widgets/visit/end-visit-prompt.test.tsx
+++ src/widgets/visit/end-visit-prompt.test.tsx
@@ -1,14 +1,31 @@
-import { endVisit } from "./visit-button-component";
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { mockCurrentVisit } from "../../../__mocks__/patient-visits.mock";
+import { EndVisit } from "./visit-button-component";
+import { visitMode, visitStatus } from "./visit-utils";
+
+const mockCurrentVisit = {
+  mode: visitMode.LOADING,
+  status: visitStatus.ONGOING,
+  visitData: {
+    visitType: {
+      uuid: "some-uuid1",
+      name: "Outpatient Visit",
+      display: "Outpatient Visit"
+    },
+    startDatetime: new Date(),
+    uuid: "some-uuid1",
+    location: {
+      uuid: "b1a8b05e-3542-4037-bbd3-998ee9c40574",
+      display: "Inpatient Ward",
+      name: "Inpatient Ward"
+    },
+    encounters: []
+  }
+};
 
 describe("End Visit Prompt", () => {
-  it("Renders End Visit Prompt", () => {
-    const endVisitPrompt = endVisit(mockCurrentVisit);
-    const element = React.createElement("div", null, endVisitPrompt);
-
-    render(element);
+  it("Renders end visit prompt", () => {
+    render(<EndVisit currentVisit={mockCurrentVisit} />);
 
     expect(
       screen.getByText("Are you sure you wish to end this visit?")
