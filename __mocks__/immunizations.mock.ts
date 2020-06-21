00b43d07cc4b8f516563482e30aa937eb3dc1491
MD Dubey <mddubey409@gmail.com>
2020-06-21 17:53:14 +0530

    Add test for ImmunizationDetailedSummary

--- __mocks__/immunizations.mock.ts
+++ __mocks__/immunizations.mock.ts
@@ -1,74 +1,93 @@
-import { openmrsFetch } from "@openmrs/esm-api";
-import { of } from "rxjs";
-
-export function performPatientImmunizationsSearch(
-  patientIdentifier: string,
-  abortController: AbortController
-) {
-  return Promise.resolve(mockPatientImmunizationsSearchResponse);
-}
-
-export function getImmunizationByUuid(immunizationUuid: string) {
-  return of(
-    mockPatientImmunizationsSearchResponse.entry.find(
-      res => res.resource.id === immunizationUuid
-    )
-  );
-}
-
-export function savePatientImmunization(
-  patientImmunization,
-  patientUuid,
-  abortController
-) {
-  return Promise.resolve({ status: 201, body: "Immunization created" });
-}
-// TODO: to be removed post integration
-const mockPatientImmunizationSearchResponse = {
-  resource: {
-    resourceType: "Immunization",
-    id: "protocol",
-    uuid: "b9c21a82-aed3-11ea-b3de-0242ac130004",
-    vaccineCode: {
-      coding: [
-        {
-          system: "http://hl7.org/fhir/sid/cvx",
-          code: "104"
-        }
-      ],
-      text: "Rotavirus"
+export const patient: fhir.Patient = {
+  resourceType: "Patient",
+  id: "8673ee4f-e2ab-4077-ba55-4980f408773e",
+  extension: [
+    {
+      url:
+        "http://fhir-es.transcendinsights.com/stu3/StructureDefinition/resource-date-created",
+      valueDateTime: "2017-01-18T09:42:40+00:00"
     },
-    patient: {
-      reference: "Patient/D1A903924D4443A7A388778D77D86155"
-    },
-    encounter: {
-      reference: "Encounter/example"
-    },
-    occurrenceDateTime: "2018-06-18",
-    location: {
-      reference: "Location/1"
-    },
-    manufacturer: {
-      reference: "Organization/hl7"
-    },
-    lotNumber: "PT123F",
-    expirationDate: "2018-12-15",
-    protocolApplied: [
-      {
-        series: "2 Months",
-        occurrenceDateTime: "2018-06-18",
-        doseNumberPositiveInt: 1
-      },
-      {
-        series: "4 Months",
-        occurrenceDateTime: "2018-09-21",
-        doseNumberPositiveInt: 2
-      }
-    ]
-  }
+    {
+      url:
+        "https://purl.org/elab/fhir/StructureDefinition/Creator-crew-version1",
+      valueString: "daemon"
+    }
+  ],
+  identifier: [
+    {
+      id: "1f0ad7a1-430f-4397-b571-59ea654a52db",
+      use: "usual",
+      system: "OpenMRS ID",
+      value: "10010W"
+    }
+  ],
+  active: true,
+  name: [
+    {
+      id: "efdb246f-4142-4c12-a27a-9be60b9592e9",
+      use: "usual",
+      family: "Wilson",
+      given: ["John"]
+    }
+  ],
+  gender: "male",
+  birthDate: "1972-04-04",
+  deceasedBoolean: false,
+  address: [
+    {
+      id: "0c244eae-85c8-4cc9-b168-96b51f864e77",
+      use: "home",
+      line: ["Address10351"],
+      city: "City0351",
+      state: "State0351tested",
+      postalCode: "60351",
+      country: "Country0351"
+    }
+  ]
 };
 
-const mockPatientImmunizationsSearchResponse = {
+export const mockImmunizationConfig = {
+  immunizations: [
+    {
+      vaccineName: "Rotavirus",
+      uuid: "uuid1",
+      isSeries: true,
+      series: [
+        { label: "2 Months", value: 1 },
+        { label: "4 Months", value: 2 },
+        { label: "6 Months", value: 3 }
+      ]
+    },
+    {
+      vaccineName: "Adenovirus",
+      uuid: "uuid2",
+      isSeries: true,
+      series: [
+        { label: "Dose-1", value: 1 },
+        { label: "Dose-2", value: 2 },
+        { label: "Dose-3", value: 3 },
+        { label: "Booster-1", value: 4 }
+      ]
+    },
+    {
+      vaccineName: "Polio",
+      uuid: "uuid3",
+      isSeries: true,
+      series: [
+        { label: "2 Months", value: 1 },
+        { label: "4 Months", value: 2 },
+        { label: "6 Months", value: 3 }
+      ]
+    },
+    {
+      vaccineName: "Influenza",
+      uuid: "uuid4",
+      isSeries: false
+    }
+  ]
+};
+
+export const mockPatientImmunizationsSearchResponse = {
   resourceType: "Bundle",
   id: "b6f39678-aed3-11ea-b3de-0242ac130004",
   type: "searchset",
--- src/widgets/immunizations/immunizations-detailed-summary.test.tsx
+++ src/widgets/immunizations/immunizations-detailed-summary.test.tsx
@@ -1,84 +1,44 @@
+import {mockPatientImmunizationsSearchResponse, patient, mockImmunizationConfig} from "../../../__mocks__/immunizations.mock";
+import {render, wait} from "@testing-library/react";
+import {BrowserRouter} from "react-router-dom";
+import { useCurrentPatient, openmrsFetch } from "@openmrs/esm-api";
+import ImmunizationsDetailedSummary from "../Immunizations/immunizations-detailed-summary.component";
 import React from "react";
-import { performPatientImmunizationsSearch } from "./immunizations.resource";
-import { render, cleanup, wait } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
-import { useCurrentPatient } from "../../../__mocks__/openmrs-esm-api.mock";
-import ImmunizationsDetailedSummary from "./immunizations-detailed-summary.component";
-import {
-  patient,
-  mockPatientImmunizationsResult
-} from "../../../__mocks__/immunizations.mock";
+import {performPatientImmunizationsSearch} from "./immunizations.resource";
+
 
 const mockPerformPatientImmunizationsSearch = performPatientImmunizationsSearch as jest.Mock;
 const mockUseCurrentPatient = useCurrentPatient as jest.Mock;
+const mockOpenmrsFetch = openmrsFetch as jest.Mock;
 
 jest.mock("./immunizations.resource", () => ({
   performPatientImmunizationsSearch: jest.fn()
 }));
 
 jest.mock("@openmrs/esm-api", () => ({
-  useCurrentPatient: jest.fn()
+  useCurrentPatient: jest.fn(),
+  openmrsFetch: jest.fn()
 }));
 
 describe("<ImmunizationsDetailedSummary />", () => {
   let wrapper: any;
 
-  afterEach(cleanup);
-  beforeEach(mockPerformPatientImmunizationsSearch.mockReset);
-  beforeEach(mockUseCurrentPatient.mockReset);
-
   it("renders without dying", async () => {
     mockUseCurrentPatient.mockReturnValue([false, patient, patient.id, null]);
     mockPerformPatientImmunizationsSearch.mockReturnValue(
-      Promise.resolve(mockPatientImmunizationsResult)
+        Promise.resolve(mockPatientImmunizationsSearchResponse)
+    );
+    mockOpenmrsFetch.mockReturnValue(
+        Promise.resolve(mockImmunizationConfig)
     );
     wrapper = render(
-      <BrowserRouter>
-        <ImmunizationsDetailedSummary />
-      </BrowserRouter>
+        <BrowserRouter>
+          <ImmunizationsDetailedSummary />
+        </BrowserRouter>
     );
 
     await wait(() => {
       expect(wrapper).toBeDefined();
-    });
-  });
-
-  it("should display the patient's immunizations correctly", async () => {
-    mockUseCurrentPatient.mockReturnValue([false, patient, patient.id, null]);
-    mockPerformPatientImmunizationsSearch.mockReturnValue(
-      Promise.resolve(mockPatientImmunizationsResult)
-    );
-    wrapper = render(
-      <BrowserRouter>
-        <ImmunizationsDetailedSummary />
-      </BrowserRouter>
-    );
-
-    await wait(() => {
-      expect(wrapper).toBeDefined();
-      expect(wrapper.getByText("Renal rejection").textContent).toBeTruthy();
-      expect(wrapper.getByText("Overweight").textContent).toBeTruthy();
-      expect(wrapper.getByText("Fever").textContent).toBeTruthy();
-      expect(wrapper.getByText("Hypertension").textContent).toBeTruthy();
-    });
-  });
-
-  it("should not display the patients immunizations when no immunization is returned", async () => {
-    mockUseCurrentPatient.mockReturnValue([false, patient, patient.id, null]);
-    mockPerformPatientImmunizationsSearch.mockReturnValue(
-      Promise.resolve({ data: { total: 0 } })
-    );
-    wrapper = render(
-      <BrowserRouter>
-        <ImmunizationsDetailedSummary />
-      </BrowserRouter>
-    );
-
-    await wait(() => {
-      expect(wrapper).toBeDefined();
-      expect(
-        wrapper.getByText("No Immunizations are documented.").textContent
-      ).toBeDefined();
     });
   });
 });
