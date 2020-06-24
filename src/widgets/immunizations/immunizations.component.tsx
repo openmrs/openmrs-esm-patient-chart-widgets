41cde74d14a08c93b8acf9e220acee0113b7057e
Vasanthakumar D <vasanth.kumar@thoughtworks.com>
2020-06-24 11:41:02 +0530

    Added immunization form component fr and es translations and removed the record cmponent

--- src/widgets/immunizations/immunizations.component.tsx
+++ src/widgets/immunizations/immunizations.component.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import ImmunizationsDetailedSummary from "./immunizations-detailed-summary.component";
-import ImmunizationRecord from "./immunization-record.component";
 
 function Immunizations(props) {
   const match = useRouteMatch();
@@ -10,9 +9,6 @@ function Immunizations(props) {
     <Switch>
       <Route exact path={match.path}>
         <ImmunizationsDetailedSummary />
-      </Route>
-      <Route exact path={`${match.path}/:immunizationUuid`}>
-        <ImmunizationRecord />
       </Route>
     </Switch>
   );
