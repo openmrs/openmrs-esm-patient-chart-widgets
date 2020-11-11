8bc2a6d950766bb20da695c401ebef622b2466fe
Dennis Kigen <kigen.work@gmail.com>
2020-11-11 14:43:27 +0300

    Add SASS support

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,17 @@ module.exports = env => ({
             }
           }
         ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
       }
     ]
   },
