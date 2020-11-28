aa039b1c848d51cfa5122e59d65c6f681434fbc0
Dennis Kigen <kigen.work@gmail.com>
2020-11-29 02:22:56 +0300

    Improvements to relationships

--- src/widgets/contact-details/contact-details.component.tsx
+++ src/widgets/contact-details/contact-details.component.tsx
@@ -9,15 +9,14 @@ import {
 } from "./relationships.resource";
 import styles from "./contact-details.scss";
 
-const Address = ({ address }) => {
-  const { city, country, state, region, postalCode } = address;
+const Address: React.FC<{ address: fhir.Address }> = ({ address }) => {
+  const { city, country, postalCode, state } = address;
   return (
-    <div>
+    <div className={styles.col}>
       <p className={styles.heading}>Address</p>
       <ul>
         <li>{postalCode}</li>
         <li>{city}</li>
-        <li>{region}</li>
         <li>{state}</li>
         <li>{country}</li>
       </ul>
@@ -25,11 +24,11 @@ const Address = ({ address }) => {
   );
 };
 
-const Contact = ({ telecom }) => {
+const Contact: React.FC<{ telecom: fhir.ContactPoint[] }> = ({ telecom }) => {
   const value = telecom ? telecom[0].value : "-";
 
   return (
-    <div>
+    <div className={styles.col}>
       <p className={styles.heading}>Contact Details</p>
       <ul>
         <li>{value}</li>
@@ -38,42 +37,69 @@ const Contact = ({ telecom }) => {
   );
 };
 
-const Relationships = ({ patientId }) => {
-  const [relationships, setRelationships] = React.useState<Relationship[]>(
-    null
-  );
+const Relationships: React.FC<{ patientId: string }> = ({ patientId }) => {
+  const [relationships, setRelationships] = React.useState<
+    ExtractedRelationship[]
+  >(null);
 
   React.useEffect(() => {
     fetchPatientRelationships(patientId)
       .then(({ data: { results } }) => {
-        setRelationships(results);
+        if (results.length) {
+          setRelationships(extractRelationshipData(results));
+        }
       })
       .catch(createErrorHandler());
   }, [patientId]);
 
+  const extractRelationshipData = (
+    relationships: Relationship[]
+  ): ExtractedRelationship[] => {
+    const relationshipsData = [];
+    for (const r of relationships) {
+      if (patientId === r.personA.uuid) {
+        relationshipsData.push({
+          uuid: r.uuid,
+          display: r.personB.display,
+          relativeAge: r.personB.age,
+          relativeUuid: r.personB.uuid,
+          relationshipType: r.relationshipType.bIsToA
+        });
+      } else {
+        relationshipsData.push({
+          uuid: r.uuid,
+          display: r.personA.display,
+          relativeAge: r.personA.age,
+          relativeUuid: r.personA.uuid,
+          relationshipType: r.relationshipType.aIsToB
+        });
+      }
+    }
+    return relationshipsData;
+  };
+
   const RenderRelationships = () => {
     if (relationships.length) {
       return (
-        <div>
-          <p className={styles.heading}>Relationships</p>
-          <ul>
-            {relationships.map(r => (
-              <li key={r.uuid}>{r.display}</li>
-            ))}
-          </ul>
-        </div>
+        <ul style={{ width: "50%" }}>
+          {relationships.map(r => (
+            <li key={r.uuid} className={styles.relationship}>
+              <div>{r.display}</div>
+              <div>{r.relationshipType}</div>
+              <div>{`${r.relativeAge} ${
+                r.relativeAge === 1 ? "yr" : "yrs"
+              }`}</div>
+            </li>
+          ))}
+        </ul>
       );
     }
-    return (
-      <div>
-        <p className={styles.heading}>Relationships</p>
-        <p>-</p>
-      </div>
-    );
+    return <p>-</p>;
   };
 
   return (
-    <div className={styles.relationshipsRow}>
+    <div className={styles.col}>
+      <p className={styles.heading}>Relationships</p>
       {relationships ? (
         <RenderRelationships />
       ) : (
@@ -83,19 +109,21 @@ const Relationships = ({ patientId }) => {
   );
 };
 
-const ContactDetails = ({ address, telecom, patientId }) => {
+const ContactDetails: React.FC<ContactDetailsProps> = ({
+  address,
+  telecom,
+  patientId
+}) => {
   const currentAddress = address.find(a => a.use === "home");
 
   return (
     <div className={styles.contactDetails}>
-      <div className={styles.address}>
-        <div className={styles.row}>
-          <Address address={currentAddress} />
-          <Contact telecom={telecom} />
-        </div>
-        <div className={styles.row}>
-          <Relationships patientId={patientId} />
-        </div>
+      <div className={styles.row}>
+        <Address address={currentAddress} />
+        <Contact telecom={telecom} />
+      </div>
+      <div className={styles.row}>
+        <Relationships patientId={patientId} />
       </div>
     </div>
   );
@@ -103,6 +131,16 @@ const ContactDetails = ({ address, telecom, patientId }) => {
 
 export default ContactDetails;
 
-type ContactDetails = {
-  patient: fhir.Patient;
+type ContactDetailsProps = {
+  address: fhir.Address[];
+  telecom: fhir.ContactPoint[];
+  patientId: string;
+};
+
+type ExtractedRelationship = {
+  uuid: string;
+  display: string;
+  relativeAge: number;
+  relativeUuid: string;
+  relationshipType: string;
 };
