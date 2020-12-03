f7035459cb878ad45b77d377478bceafb0514c83
Ivange  Larry <ivangelarry@gmail.com>
2020-12-04 00:29:40 +0100

    Fix failing test

--- src/widgets/appointments/appointments.component.test.tsx
+++ src/widgets/appointments/appointments.component.test.tsx
@@ -1,14 +1,30 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
 import Appointments from "./appointments.component";
+import { MemoryRouter } from "react-router";
+
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    BrowserRouter: jest
+      .fn()
+      .mockImplementation(({ children }) => <div>{children}</div>)
+  };
+});
+
+window["getOpenmrsSpaBase"] = jest.fn().mockImplementation(() => "/");
 
 describe("<AppointmentsComponent />", () => {
   it("renders without dying", () => {
     render(
-      <BrowserRouter>
+      <MemoryRouter
+        initialEntries={["/patient/:patientUuid/chart/appointments"]}
+      >
         <Appointments />
-      </BrowserRouter>
+      </MemoryRouter>
     );
 
     expect(
