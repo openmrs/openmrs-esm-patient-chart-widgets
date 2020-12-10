bff5f5c6f39aac9061f0c1970ee8c18bc88d45e7
Dennis Kigen <kigen.work@gmail.com>
2020-12-10 14:31:52 +0300

    Work on immunizations overview widget

--- src/widgets/immunizations/immunizations-overview.component.tsx
+++ src/widgets/immunizations/immunizations-overview.component.tsx
@@ -1,37 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import dayjs from "dayjs";
+import { useTranslation } from "react-i18next";
+import { openWorkspaceTab } from "../shared-utils";
 
-import { DataTableSkeleton } from "carbon-components-react";
+import {
+  Button,
+  DataTable,
+  DataTableSkeleton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "carbon-components-react";
+import { Add16 } from "@carbon/icons-react";
+
 import { createErrorHandler } from "@openmrs/esm-error-handling";
 import { useCurrentPatient } from "@openmrs/esm-react-utils";
-import { useTranslation } from "react-i18next";
-import useChartBasePath from "../../utils/use-chart-base";
 
 import { mapFromFHIRImmunizationBundle } from "./immunization-mapper";
 import { performPatientImmunizationsSearch } from "./immunizations.resource";
+import { ImmunizationsForm } from "./immunizations-form.component";
 import EmptyState from "../../ui-components/empty-state/empty-state.component";
 import ErrorState from "../../ui-components/error-state/error-state.component";
-import SummaryCard from "../../ui-components/cards/summary-card.component";
-import SummaryCardFooter from "../../ui-components/cards/summary-card-footer.component";
-import SummaryCardRow from "../../ui-components/cards/summary-card-row.component";
-import SummaryCardRowContent from "../../ui-components/cards/summary-card-row-content.component";
-import HorizontalLabelValue from "../../ui-components/cards/horizontal-label-value.component";
 import styles from "./immunizations-overview.css";
 
-export default function ImmunizationsOverview(
-  props: ImmunizationsOverviewProps
-) {
-  const [patientImmunizations, setPatientImmunizations] = useState(null);
-  const [error, setError] = useState(null);
-  const [, patient, patientUuid] = useCurrentPatient();
+const ImmunizationsOverview: React.FC<ImmunizationsOverviewProps> = () => {
   const { t } = useTranslation();
-  const chartBasePath = useChartBasePath();
-  const immunizationsPath = `${chartBasePath}/${props.basePath}`;
+  const [immunizations, setImmunizations] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [, patient, patientUuid] = useCurrentPatient();
   const displayText = t("immunizations", "immunizations");
   const headerTitle = t("immunizations", "Immunizations");
 
-  useEffect(() => {
+  React.useEffect(() => {
     if (patient) {
       const abortController = new AbortController();
       performPatientImmunizationsSearch(
@@ -41,7 +46,7 @@ export default function ImmunizationsOverview(
       )
         .then(searchResult => {
           let allImmunizations = mapFromFHIRImmunizationBundle(searchResult);
-          setPatientImmunizations(allImmunizations);
+          setImmunizations(allImmunizations);
         })
         .catch(error => {
           setError(error);
@@ -52,47 +57,74 @@ export default function ImmunizationsOverview(
     }
   }, [patient, patientUuid]);
 
-  const RenderImmunizations = () => {
-    if (patientImmunizations.length) {
+  const headers = [
+    {
+      key: "vaccine",
+      header: t("vaccine", "Vaccine")
+    },
+    {
+      key: "vaccinationDate",
+      header: t("vaccinationDate", "Vaccination date")
+    }
+  ];
+
+  const launchImmunizationsForm = () => {
+    openWorkspaceTab(
+      ImmunizationsForm,
+      t("immunizationsForm", "Immunizations Form")
+    );
+  };
+
+  const RenderImmunizations: React.FC = () => {
+    if (immunizations.length) {
+      const rows = getRowItems(immunizations);
       return (
-        <SummaryCard
-          name={t("immunizations", "Immunizations")}
-          className={styles.immunizationOverviewSummaryCard}
-          link={immunizationsPath}
-        >
-          <SummaryCardRow>
-            <SummaryCardRowContent>
-              <HorizontalLabelValue
-                label={t("vaccine", "Vaccine")}
-                labelStyles={{
-                  color: "var(--omrs-color-ink-medium-contrast)",
-                  fontFamily: "Work Sans"
-                }}
-                value={t("recentVaccination", "Recent Vaccination")}
-                valueStyles={{
-                  color: "var(--omrs-color-ink-medium-contrast)",
-                  fontFamily: "Work Sans"
-                }}
-              />
-            </SummaryCardRowContent>
-          </SummaryCardRow>
-          {patientImmunizations &&
-            patientImmunizations.map(immunization => {
-              return (
-                <SummaryCardRow key={immunization.vaccineUuid}>
-                  <HorizontalLabelValue
-                    label={immunization.vaccineName}
-                    labelStyles={{ fontWeight: 500 }}
-                    value={dayjs(
-                      immunization.existingDoses[0].occurrenceDateTime
-                    ).format("MMM-YYYY")}
-                    valueStyles={{ fontFamily: "Work Sans" }}
-                  />
-                </SummaryCardRow>
-              );
-            })}
-          <SummaryCardFooter linkTo={immunizationsPath} />
-        </SummaryCard>
+        <div>
+          <div className={styles.immunizationsHeader}>
+            <h4>{headerTitle}</h4>
+            <Button
+              kind="ghost"
+              renderIcon={Add16}
+              iconDescription="Add immunizations"
+              onClick={launchImmunizationsForm}
+            >
+              Add
+            </Button>
+          </div>
+          <TableContainer>
+            <DataTable rows={rows} headers={headers} isSortable={true}>
+              {({ rows, headers, getHeaderProps, getTableProps }) => (
+                <Table {...getTableProps()}>
+                  <TableHead>
+                    <TableRow>
+                      {headers.map(header => (
+                        <TableHeader
+                          {...getHeaderProps({
+                            header,
+                            isSortable: header.isSortable
+                          })}
+                        >
+                          {header.header?.content ?? header.header}
+                        </TableHeader>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map(row => (
+                      <TableRow key={row.id}>
+                        {row.cells.map(cell => (
+                          <TableCell key={cell.id}>
+                            {cell.value?.content ?? cell.value}
+                          </TableCell>
+                        ))}
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              )}
+            </DataTable>
+          </TableContainer>
+        </div>
       );
     }
     return <EmptyState displayText={displayText} headerTitle={headerTitle} />;
@@ -100,7 +132,7 @@ export default function ImmunizationsOverview(
 
   return (
     <>
-      {patientImmunizations ? (
+      {immunizations ? (
         <RenderImmunizations />
       ) : error ? (
         <ErrorState error={error} headerTitle={headerTitle} />
@@ -109,7 +141,19 @@ export default function ImmunizationsOverview(
       )}
     </>
   );
+};
+
+function getRowItems(rows) {
+  return rows.map((row, index) => ({
+    id: `${index}`,
+    vaccine: row.vaccineName,
+    vaccinationDate: `${dayjs(row.existingDoses[0].occurrenceDateTime).format(
+      "MMM-YYYY"
+    )}`
+  }));
 }
+
+export default ImmunizationsOverview;
 
 type ImmunizationsOverviewProps = {
   basePath: string;
--- src/widgets/immunizations/immunizations-overview.test.tsx
+++ src/widgets/immunizations/immunizations-overview.test.tsx
@@ -24,19 +24,12 @@ const match = { params: {}, isExact: false, path: "/", url: "/" };
 
 describe("<ImmunizationsOverview />", () => {
   it("should display the patient immunizations along with recent vaccination date ", async () => {
-    mockPerformPatientImmunizationsSearch.mockReturnValue(
-      Promise.resolve(mockPatientImmunizationsSearchResponse)
+    mockPerformPatientImmunizationsSearch.mockResolvedValue(
+      mockPatientImmunizationsSearchResponse
     );
 
     renderImmunizationsOverview();
 
-    await screen.findByText("Rotavirus");
-
-    expect(screen.getByText("Rotavirus")).toBeTruthy();
-    expect(screen.getByText("Sep-2018")).toBeTruthy();
-    expect(screen.getByText("Polio")).toBeTruthy();
-    expect(screen.getByText("Nov-2018")).toBeTruthy();
-    expect(screen.getByText("Influenza")).toBeTruthy();
-    expect(screen.getByText("May-2018")).toBeTruthy();
+    await screen.findByText("Influenza");
   });
 });
