33b331b54db1ec402a4e64f63dc44a2f617778ba
Ian <ian_bacher@brown.edu>
2021-02-09 13:59:49 -0500

    MF-450: Active Visit tag should be visible when there is an active visit

--- src/widgets/banner/patient-banner.test.tsx
+++ src/widgets/banner/patient-banner.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import PatientBanner from "./patient-banner.component";
+import { getVisitsForPatient } from "../visit/visit.resource";
+import { of } from "rxjs";
 
 jest.unmock("lodash");
 const lodash = jest.requireActual("lodash");
@@ -8,27 +10,81 @@ lodash.capitalize = jest
   .fn()
   .mockImplementation(s => s.charAt(0).toUpperCase() + s.slice(1));
 
+const mockGetVisitsForPatient = getVisitsForPatient as jest.Mock;
+
+jest.mock("../visit/visit.resource", () => ({
+  getVisitsForPatient: jest.fn()
+}));
+
 function renderPatientBanner() {
   render(<PatientBanner />);
 }
 
-it("clicking the button toggles displaying the patient's contact details", () => {
-  renderPatientBanner();
+describe("<PatientBanner />", () => {
+  afterEach(mockGetVisitsForPatient.mockReset);
 
-  const showContactDetailsBtn = screen.getByRole("button", {
-    name: "Show Contact Details"
+  beforeEach(() => {
+    mockGetVisitsForPatient.mockReturnValue(of({}));
   });
 
-  fireEvent.click(showContactDetailsBtn);
+  it("clicking the button toggles displaying the patient's contact details", () => {
+    renderPatientBanner();
 
-  const hideContactDetailsBtn = screen.getByRole("button", {
-    name: "Hide Contact Details"
+    const showContactDetailsBtn = screen.getByRole("button", {
+      name: "Show Contact Details"
+    });
+
+    fireEvent.click(showContactDetailsBtn);
+
+    const hideContactDetailsBtn = screen.getByRole("button", {
+      name: "Hide Contact Details"
+    });
+    expect(hideContactDetailsBtn).toBeInTheDocument();
+    expect(screen.getByText("Address")).toBeInTheDocument();
+    expect(screen.getByText("Contact Details")).toBeInTheDocument();
+
+    fireEvent.click(hideContactDetailsBtn);
+
+    expect(showContactDetailsBtn).toBeInTheDocument();
   });
-  expect(hideContactDetailsBtn).toBeInTheDocument();
-  expect(screen.getByText("Address")).toBeInTheDocument();
-  expect(screen.getByText("Contact Details")).toBeInTheDocument();
 
-  fireEvent.click(hideContactDetailsBtn);
+  it("should display the Active Visit tag when there is an active visit", () => {
+    mockGetVisitsForPatient.mockReturnValue(
+      of({
+        ok: true,
+        data: {
+          results: [
+            {
+              uuid: "4d4fd38c-8729-4e2f-874d-bbac83dd38d0",
+              stopDatetime: null
+            }
+          ]
+        }
+      })
+    );
 
-  expect(showContactDetailsBtn).toBeInTheDocument();
+    renderPatientBanner();
+
+    expect(screen.queryByTitle("Active Visit")).toBeVisible();
+  });
+
+  it("should not display the Active Visit tag when there is not an active visit", () => {
+    mockGetVisitsForPatient.mockReturnValue(
+      of({
+        ok: true,
+        data: {
+          results: [
+            {
+              uuid: "aeef2554-5f75-4507-8236-e5aface1c0a1",
+              stopDatetime: "2020-07-28T10:29:00.000+0000"
+            }
+          ]
+        }
+      })
+    );
+
+    renderPatientBanner();
+
+    expect(screen.queryByTitle("Active Visit")).toBeNull();
+  });
 });
