a617f18220ae01752a69cdb9a2b52637fc2f3901
Nikita Malyschkin <44928856+nmalyschkin@users.noreply.github.com>
2021-03-08 14:57:34 +0100

    [test-results] add loadPatientData and Overview component (#207)
    * [test-results] add loadPatientData and Overview component
    
    * move constant to top
    
    * add review feedback

---
+++ src/widgets/test-results/loadPatientTestData/usePatientResultsData.ts
@@ -0,0 +1,30 @@
+import React from "react";
+
+import loadPatientData from "./loadPatientData";
+import { PatientData } from "./types";
+
+type LoadingState = {
+  sortedObs: PatientData;
+  loaded: boolean;
+  error: Object | undefined;
+};
+
+const usePatientResultsData = (patientUuid: string): LoadingState => {
+  const [state, setState] = React.useState<LoadingState>({
+    sortedObs: {},
+    loaded: false,
+    error: undefined
+  });
+
+  React.useEffect(() => {
+    loadPatientData(patientUuid)
+      .then(sortedObs =>
+        setState({ sortedObs, loaded: true, error: undefined })
+      )
+      .catch(error => setState({ ...state, loaded: true, error }));
+  }, []);
+
+  return state;
+};
+
+export default usePatientResultsData;
